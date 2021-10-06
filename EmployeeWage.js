8a632f5b9b3667e4b0598c032c18005d932a3799
Prashanth <imprash4u@gmail.com>
2021-10-06 19:31:14 +0530

    Refactored: Writing the code using Function To get Employee Work Hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,18 +6,24 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 
-let empHrs = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-switch (employeeCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
-    default:
-        empHrs = 0;
+
+function getWorkingHrs(employeeCheck) {
+    switch (employeeCheck) {
+        case IS_PART_TIME:
+            console.log("Employee Worked Part Time");
+            empHrs = PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            console.log("Employee Worked Full Time");
+            empHrs = FULL_TIME_HRS;
+            break;
+        default:
+            console.log("Employee is Absent");
+            empHrs = 0;
+    }
 }
 
+let employeeCheck = Math.floor(Math.random() * 10) % 3;
+getWorkingHrs(employeeCheck);
 let empWage = empHrs * WAGE_PER_HR;
-console.log("Employee Wage is: "+empWage);
+console.log("Employee Wage is: " + empWage);
